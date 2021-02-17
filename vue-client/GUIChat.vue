a7e7a0dbabab8bb08b6e3d8e786e174e569f42a1
DanielLepeVega <danylepevega@gmail.com>
2021-02-16 19:59:29 -0600

    component message board start
    Signed-off-by: DanielLepeVega <danylepevega@gmail.com>

---
+++ vue-client/GUIChat.vue
@@ -0,0 +1,18 @@
+<template>
+    <message-board></message-board>
+</template>
+
+<script>
+
+import MessageBoard from './components/MessageBoard.vue';
+
+export default {
+    
+
+
+    components:{
+        MessageBoard
+    }
+
+}
+</script>
---
+++ vue-client/components/MessageBoard.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-row justify="center">
+    <v-col
+      cols="12"
+      sm="8"
+      md="6"
+    >
+      <v-card>
+        <v-toolbar
+          color="blue-grey darken-3"
+          dark
+        >
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+          <v-toolbar-title>Message Board</v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+        </v-toolbar>
+
+        <v-list two-line>
+          <template v-for="(item, index) in items">
+            <v-subheader
+              v-if="item.header"
+              :key="item.header"
+              inset
+            >
+              {{
+                item.header
+              }}
+            </v-subheader>
+
+            <v-divider
+              v-else-if="item.divider"
+              :key="index"
+              inset
+            ></v-divider>
+
+            <v-list-item
+              v-else
+              :key="item.title"
+              ripple
+            >
+              <v-list-item-avatar>
+                <img :src="item.avatar">
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title v-html="item.title"></v-list-item-title>
+                <v-list-item-subtitle
+                  v-html="item.subtitle"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-list>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
