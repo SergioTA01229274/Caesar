a2967eb86592005cdaaca05bfd4ce24e15254814
Sergio Tostado <a01229274@itesm.mx>
2021-02-22 20:11:08 -0600

    Login view ready :)

--- vue/src/router/router.js
+++ vue/src/router/router.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomePage from '../components/HomePage'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'HomePage',
-    component: HomePage
-  },
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/login.vue')
+  }
 ]
 
 const router = new VueRouter({
