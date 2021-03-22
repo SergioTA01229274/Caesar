2c5c3a188c55104f33233444e7312a613e770627
DanielLepeVega <danylepevega@gmail.com>
2021-03-22 16:56:12 -0600

    Authentication and routes, moving to server
    Signed-off-by: DanielLepeVega <danylepevega@gmail.com>

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -18,24 +18,3 @@
   color: #2c3e50;
 }
 </style>
-
-<!--<script>
-  "use strict";
-  const express = require('express');
-  const DB = require('./db');
-  const config = require('./config');
-  const bcrypt = require('bcrypt');
-  const jwt = require('jsonwebtoken');
-  const bodyParser = require('body-parser');
-
-  const db = new DB("sqlitedb")
-  const app = express();
-  const router = express.Router();
-
-  router.use(bodyParser.urlencoded({ extended: false }));
-  router.use(bodyParser.json());
-
-  export default {
-    
-  }
-</script>-->
