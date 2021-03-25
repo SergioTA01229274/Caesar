528614fd21790bf5560aef7ed9e1c97e2e82d0a4
DanielLepeVega <danylepevega@gmail.com>
2021-03-25 14:26:02 -0600

    cosas
    Signed-off-by: DanielLepeVega <danylepevega@gmail.com>

--- vue/src/components/chatSection.vue
+++ vue/src/components/chatSection.vue
@@ -3,7 +3,7 @@
       <section class="chat-box">
         <div class="chat-box-list-container">
           <ul class="chat-box-list" >
-            <li class="message" v-for="msg in msgInfo" :key="msg">{{msg.msg}}</li>
+            <li class="messageDisplayed" v-for="msg in msgInfo" :key="msg" v-bind:class="{user: msg.sender}" v-if="user === 'msg.sender'">{{msg.sender}}: {{msg.msg}}</li>
           </ul>
         </div>
       </section>
@@ -15,13 +15,16 @@
         background-color: #FFFFFF;
         height: 31rem;
     }
+    .messageDisplayed {
+      background-color: aqua;
+      text-align: left-; 
+    }
 </style>
 
 <script>
   export default {
     name:"chatSection",
     props: ['msgInfo'],
-    watch: { msgInfo: { inmediate: true, handler(val, oldVal){console.log(val);}}}
-
+    watch: { msgInfo: { inmediate: true, handler(val, oldVal){console.log(val);}}},
   }
 </script>
