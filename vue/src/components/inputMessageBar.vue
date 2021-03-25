ec3702692e968ff35d18e2edca44e33b90a6cd42
Andres Salas Villarreal <andres-salas@hotmail.com>
2021-03-25 00:40:03 -0600

    Key Up
    enter

--- vue/src/components/inputMessageBar.vue
+++ vue/src/components/inputMessageBar.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <v-row>
-            <v-col cols="10"><v-text-field id="textInput" color="custom" label="Type a message" v-model="tmpMessage" solo></v-text-field></v-col>
+            <v-col cols="10"><v-text-field id="textInput" color="custom" label="Type a message" v-model="tmpMessage" @keyup.enter="sendMsgEvent()" solo></v-text-field></v-col>
             <v-col cols="2" id="buttons"><v-btn large class="inputButton" id="fileButton" @click="showReq()"><v-icon>mdi-paperclip</v-icon>
-            </v-btn> <v-btn large id="sendButton" class="inputButton" @click="sendMsgEvent()"><v-icon id="sendIcon">mdi-send</v-icon></v-btn>
+            </v-btn> <v-btn large id="sendButton" class="inputButton"  @click="sendMsgEvent()"><v-icon id="sendIcon">mdi-send</v-icon></v-btn>
             </v-col>
         </v-row>
     </div>
