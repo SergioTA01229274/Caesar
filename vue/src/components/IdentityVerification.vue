d1227bbf985afe335a7f636445fb805d907a3705
Andres Salas Villarreal <andres-salas@hotmail.com>
2021-04-05 18:30:43 -0500

    Package Identity
    Read and Validate the package identity.
    
    Missing local storage of values

--- vue/src/components/IdentityVerification.vue
+++ vue/src/components/IdentityVerification.vue
@@ -2,12 +2,31 @@
     <div id="identityVerification-app">
         <v-container id='container' fluid>
             <div id="subContainer">
-                <v-row id="keysRow">
+                <v-row no-gutters>
+                    <v-col cols="9">
+                        <v-file-input 
+                            prepend-icon=mdi-account-key 
+                            v-model="key" 
+                            accept=".json" 
+                            label="Key.json" 
+                            @change="previewFiles" 
+                            @keyup.enter="verifyJSON" 
+                            truncate-length="15"> 
+                        </v-file-input>
+                    </v-col>
+                </v-row>
+                <!--<v-row id="keysRow">
                     <v-col><v-textarea v-model="loginKeyToVerify" label="Login Key" id="loginKeyArea" no-resize></v-textarea></v-col>
                     <v-col> <v-textarea v-model="privateKeyToLoad" label="Private Key to load" id="privateKeyArea" no-resize></v-textarea></v-col>
-                </v-row>
-            <v-row id="btnRow">
-                <v-btn id = 'submitButton' @click="verifyKeys()">
+                </v-row>-->
+                <v-row id="btnRow">
+                    <v-btn 
+                        id = 'submitButton' 
+                        class = 'buttonLabel' 
+                        label="submitButtonFor" 
+                        :disabled="!isFormValid"
+                        @click="verifyKeys()"
+                    >
                     <label for="submitButtonFor" class = 'buttonLabel'>
                         Submit
                     </label>
@@ -25,10 +44,62 @@ export default {
     data() {
         return {
             loginKeyToVerify: '',
-            privateKeyToLoad: ''
+            privateKeyToLoad: '',
+            isFormValid: false,
+            key: {
+                public: {
+                    n: ' ',
+                    e: ' '
+                },
+                private: {
+                    login: ' ', 
+                    n: ' ',
+                    d: ' '
+                }
+            },
         }
     },
     methods: {
+        previewFiles: function(files) {
+            if (files != null) {
+                const reader = new FileReader();
+                var result;
+                reader.onload = (event) => {
+                    this.parseKeys(event.target.result);
+                }
+                reader.readAsText(files);
+            } else {
+                this.keys();
+            }
+        },
+        parseKeys(result){
+            var keyObj = JSON.parse(result);
+            try {
+                Object.assign(this.key, keyObj);
+            } catch (error) {
+                this.isFormValid = false;
+            }
+            if(this.key.private.login != ''){
+                this.loginKeyToVerify = this.key.private.login;
+                this.isFormValid = true;
+            } else {
+                this.isFormValid = false;
+            }
+        },
+        keys(){
+            this.key = {
+                public: {
+                        n: '',
+                        e: ''
+                    },
+                    private: {
+                        login: '', 
+                        n: '',
+                        d: ''
+                    }
+            }
+            this.isFormValid = false;
+        },
         verifyKeys(){
             let body = {username: localStorage.username, loginKey: this.loginKeyToVerify};
             console.log(body);
