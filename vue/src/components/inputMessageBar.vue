d9f88711e846d4db49a6a9f076c0b7963b9e1bc9
Sergio Tostado <a01229274@itesm.mx>
2021-05-01 13:15:25 -0500

    Minor frontend fixes

--- vue/src/components/inputMessageBar.vue
+++ vue/src/components/inputMessageBar.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <v-row>
-            <v-col cols="10"><v-text-field id="textInput" color="custom" label="Type a message" v-model="tmpMessage" @keyup.enter="sendMsgEvent()" solo></v-text-field></v-col>
-            <v-col cols="2" id="buttons"><v-btn large class="inputButton" id="fileButton" @click="showReq()"><v-icon>mdi-paperclip</v-icon>
-            </v-btn> <v-btn large id="sendButton" class="inputButton"  @click="sendMsgEvent()"><v-icon id="sendIcon">mdi-send</v-icon></v-btn>
+            <v-col cols="11"><v-text-field id="textInput" color="custom" label="Type a message" v-model="tmpMessage" @keyup.enter="sendMsgEvent()" solo></v-text-field></v-col>
+            <v-col cols="1" id="buttons">
+                <v-btn large id="sendButton" class="inputButton"  @click="sendMsgEvent()"><v-icon id="sendIcon">mdi-send</v-icon></v-btn>
             </v-col>
         </v-row>
     </div>
@@ -12,14 +12,10 @@
 <style scoped>
     
     #sendButton {
-        margin-left: 0rem;
         background-color: #00796B !important;
     }
     #sendIcon {
         color: #E5E5E5;
-    }
-    #fileButton {
-        background-color: #7983A6;
     }
     .custom {
         background-color: #E5E5E5;
